4ddc4b5a1142c748b3dcf860e3457df124a4fa62
idgm5 <idgm@pm.me>
2020-06-29 17:34:15 -0500

    Add Array Analysis tests

---
+++ modules/analysis.js
@@ -0,0 +1,11 @@
+function Analysis(...array) {
+  var avg = array.reduce((a,b) => (a+b)) / array.length;
+  return {
+    average: avg,
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length
+  }
+};
+
+module.exports = Analysis;
