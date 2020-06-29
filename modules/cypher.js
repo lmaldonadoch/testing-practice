1e6050e2e8701a39696f2e90defd7e6992211ece
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-06-29 16:58:49 -0500

    Finished calculator and cypher

--- modules/cypher.js
+++ modules/cypher.js
@@ -1,15 +1,14 @@
 const Cypher = (() => {
   const encryptUpper = (unicode, offset) => {
     if (unicode + offset > 90) {
-      return unicode + offset - 90 + 65;
+      return unicode + offset - 90 + 64;
     }
     return unicode + offset;
   };
 
   const encryptLower = (unicode, offset) => {
     if (unicode + offset > 122) {
-      console.log(unicode + offset - 122 + 97);
-      return unicode + offset - 122 + 97;
+      return unicode + offset - 122 + 96;
     }
     return unicode + offset;
   };
@@ -17,20 +16,9 @@ const Cypher = (() => {
   const encrypt = (offset, string) => {
     let array = string.split('');
     let encryptedArray = array.map((elem) => {
-      if (
-        elem.charCodeAt(0) + offset >= 65 &&
-        elem.charCodeAt(0) + offset <= 90
-      ) {
+      if (elem.charCodeAt(0) >= 65 && elem.charCodeAt(0) <= 90) {
         return String.fromCharCode(encryptUpper(elem.charCodeAt(0), offset));
-      } else if (
-        elem.charCodeAt(0) + offset >= 97 &&
-        elem.charCodeAt(0) + offset <= 122
-      ) {
-        console.log(elem);
-        // console.log(
-        //   String.fromCharCode(encryptLower(elem.charCodeAt(0), offset))
-        // );
-
+      } else if (elem.charCodeAt(0) >= 97 && elem.charCodeAt(0) <= 122) {
         return String.fromCharCode(encryptLower(elem.charCodeAt(0), offset));
       } else {
         return elem;
--- tests/cypher.test.js
+++ tests/cypher.test.js
@@ -5,5 +5,13 @@ it('Encrypt message all lower case', () => {
 });
 
 it('Encrypt message all upper case', () => {
-  expect(Cypher.encrypt(2, 'ABC')).toBe('cde');
+  expect(Cypher.encrypt(2, 'ABC')).toBe('CDE');
+});
+
+it('Encrypt message with numbers', () => {
+  expect(Cypher.encrypt(2, 'a5c8b9')).toBe('c5e8d9');
+});
+
+it('Encrypt message that has to start on a after z', () => {
+  expect(Cypher.encrypt(2, 'yza')).toBe('abc');
 });
